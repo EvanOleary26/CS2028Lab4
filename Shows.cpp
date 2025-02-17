7314ae8bf8d2fc970400e7815dda7065eefbb08f
ihages <hagemaij@mail.uc.edu>
2025-02-17 10:47:39 -0500

    updates to displaying the show details for when run as a show--WIP
    I need to figure out how to access the members directly for the overall show title and desc.

--- Shows.cpp
+++ Shows.cpp
@@ -3,10 +3,13 @@
 
 void Shows::Play() {
     std::cout << "Playing: " << title << std::endl;
+    Details();
 }
 
 
 void Shows::Details() {
+    std::string title = getTitle();
+    std::string description = getDesc();
     std::cout << "Title: " << title << std::endl;
     std::cout << "Description: " << description << std::endl; 
 }
